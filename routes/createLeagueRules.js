670dce356ff6b9e00a6f459e6ed14d8aa0ac87d9
Jackie Pham <jak464@bu.edu>
2016-10-03 22:46:04 -0400

    rule stuf

--- routes/createLeagueRules.js
+++ routes/createLeagueRules.js
@@ -1,10 +1,33 @@
 /**
  * Created by jackie on 10/2/16.
  */
-/**
- * Created by jackie on 10/2/16.
- */
+var DB = require('../schemaDb');
+var League = DB.getLeagueModel();
+var LeagueRules = DB.getLeagueRulesModel();
+
 module.exports =
-    function findLeague(req , res){
-        res.render('createLeagueRuleView');
-    };
+    function createLeagueRules(req , res) {
+        var name = req.body;
+
+        League.findOne({"leagueOwner" : req.user._id},function (err, league) {
+            if (err) return handleError(err);
+            console.log(league._id);
+        });
+
+    }
+// run through all the parameters and get them 2 at a time
+
+/**
+ * var arr = // array of objects;
+ res = [];
+
+ arr.forEach(function (item) {
+    item.save(function (err) {
+        res.push(err);
+        if (res.length === arr.length)
+        {
+            // Done
+        }
+    });
+});
+ */
